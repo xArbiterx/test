4c4fc25a8312025989042c4b5923e56cd35bfef2
arbiter <arbiterqq@gmail.com>
2021-11-19 17:35:12 +0800

    Add two converters

---
+++ LabTools/XmlEntityConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTools
+{
+    public class XmlEntityConverter
+    {
+        //In Visual Sudio, click Edit >> Paste Special >> Paste XML As Classes.
+    }
+}
