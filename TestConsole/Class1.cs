fec9d0346fdfc24ecca009968428c1622af8c160
arbiter <arbiterqq@gmail.com>
2021-12-27 17:55:44 +0800

    test console

---
+++ TestConsole/Class1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Class1
+    {
+        private const string PROP = "prop";
+        private const string CLASS = "class";
+        public static void Test1()
+        {
+            string xml = "<xxx><xxx1></xxx1><xxx11></xxx11><xxxx/></xxx>";
+            xml = xml.Trim();
+            Queue<string> nameTextQueue = new();
+
+            int start = 0, end = 0;
+            bool sFlag = false;
+            for (int i = 0; i < xml.Length; i++)
+            {
+                var word = xml[i];
+
+                if (word == '<')
+                {
+                    if (sFlag)
+                    {
+                        throw new Exception("");
+                    }
+                    start = i + 1;
+                    sFlag = true;
+                }
+                else if (word == '>')
+                {
+                    if (!sFlag)
+                    {
+                        throw new Exception("");
+                    }
+                    end = i;
+                    var tagText = xml[start..end].Trim(' ');
+                    if (tagText.Length == 0)
+                    {
+                        throw new Exception();
+                    }
+                    nameTextQueue.Enqueue(tagText);
+                    sFlag = false;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            //当前对象栈 (节点名称)
+            Stack<string> stack = new();
+            // 字典（父节点名称，（栈（子节点名称，类型<字段或者对象>）））
+            List<KeyValuePair<string, KeyValuePair<string, string>>> dic = new();
+
+            Stack<string> currStack = new();
+            string lastName = "";
+            while (nameTextQueue.Count > 0)
+            {
+                var currText = nameTextQueue.Dequeue();
+                var textArr = currText.Split(" ");
+
+                if (currText.Last() == '/')
+                {
+                    dic.Add(new KeyValuePair<string, KeyValuePair<string, string>>(stack.Peek(), new KeyValuePair<string, string>(textArr[0].Replace("/>", ""), PROP)));
+                }
+                else if (!textArr[0].Contains("/"))
+                {
+                    currStack.Push(textArr[0]);
+                    if (lastName == "")
+                    {
+                        lastName = textArr[0];
+                    }
+                    else
+                    {
+                        if (lastName != textArr[0])
+                        {
+                            if (stack.Count == 0 || stack.Peek() != lastName)
+                            {
+                                stack.Push(lastName);
+                            }
+                        }
+                    }
+                }
+                else if (textArr[0] == "/" + currStack.Peek())
+                {
+                    //是则是属性，否则是class
+                    if (currStack.Peek() == lastName)
+                    {
+                        if (stack.Count > 0)
+                        {
+                            dic.Add(new KeyValuePair<string, KeyValuePair<string, string>>(stack.Peek(), new KeyValuePair<string, string>(textArr[0], PROP)));
+                        }
+                    }
+                    else
+                    {
+                        if (stack.Count > 0 && stack.Peek() == currStack.Peek())
+                        {
+                            stack.Pop();
+                        }
+
+                        if (stack.Count > 0)
+                        {
+                            dic.Add(new KeyValuePair<string, KeyValuePair<string, string>>(stack.Peek(), new KeyValuePair<string, string>(currStack.Pop(), PROP)));
+                            lastName = stack.Peek();
+                        }
+                        else
+                        {
+                            dic.Add(new KeyValuePair<string, KeyValuePair<string, string>>("", new KeyValuePair<string, string>(currStack.Pop(), CLASS)));
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            int a = 0;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Text.RegularExpressions;
+using TestConsole;
+
+Class1.Test1();
